48d929410eafd248527261919b49b366f3b8b5a4
asmanov <asmanov.a@gmail.com>
2023-04-01 18:40:36 +0300

    Added frontend for game "TicTacToe"

--- TicTacToe_Command/Form1.cs
+++ TicTacToe_Command/Form1.cs
@@ -12,9 +12,53 @@ namespace TicTacToe_Command
 {
     public partial class Form1 : Form
     {
+        private int posX = 30;
+        private int posY = 30;
+        private int flag = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // Создает поле игры
+        private void NewGame(object sender, System.EventArgs e)
+        { 
+            if(flag > 0)
+            {
+                for (int f = 10; f > 1; f--)
+                {
+                    this.Controls.RemoveAt(f);
+                }
+            }
+            
+            flag = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    int x = posX + i * 110;
+                    int y = posY + j * 110;
+                    Button btn = new Button();
+                    btn.Enabled = true;
+                    btn.Size = new Size(100, 100);
+                    btn.BackColor = Color.FromArgb(150, 0, 155, 0);
+                    btn.Location = new Point(x, y);
+                    btn.Font = new Font("Ubuntu", 60);
+                    btn.Click += Btn_Click;
+                    this.Controls.Add(btn);
+                }
+            }   
+        }
+        //ставит Х или О при нажатии на кнопку
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            if(flag%2 == 0)
+            {
+                (sender as Button).Text = "X";
+            }
+            else (sender as Button).Text = "O";
+            flag++;
+           (sender as Button).Enabled = false;
         }
     }
 }
